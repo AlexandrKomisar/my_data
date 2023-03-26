4acc5e6f2d5e7dad173a7842d9e11ee71134bcb0
AlexandrKomisar <monolitalex@gmail.com>
2023-03-26 23:06:17 +0300

    data

---
+++ js/my_data.js
@@ -0,0 +1,19 @@
+let myData = [
+  [
+    (year = 2020),
+    (water = [456,346,778]),
+    (gas = [34,36,37]),
+    (electricity = []),
+    (rent = []),
+    (internet = [165, 165]),
+  ],
+  [
+    (year = 2021),
+    (water = []),
+    (gas = []),
+    (electricity = []),
+    (rent = []),
+    (internet = [165, 165]),
+  ],
+];
+console.log(myData);
