0b831cf29a7c3d33e96f645d036381653729fd29
AlexandrKomisar <monolitalex@gmail.com>
2023-03-27 23:25:13 +0300

    arr

--- js/my_data.js
+++ js/my_data.js
@@ -1,7 +1,7 @@
 let myData = [
   [
     (year = 2020),
-    (water = [456,346,778]),
+    (water = [450,456,546,778]),
     (gas = [34,36,37]),
     (electricity = []),
     (rent = []),
@@ -16,4 +16,14 @@ let myData = [
     (internet = [165, 165]),
   ],
 ];
-console.log(myData);
+
+let sumWater = 0;
+let totalWater = 0;
+for (let i = 0; i < myData[0][1].length; i++){
+  console.log(myData[0][1][0]);
+}
+
+console.log(myData[0][1]);
+console.log(sumWater);
+console.log("totalWater -- "+totalWater);
+
