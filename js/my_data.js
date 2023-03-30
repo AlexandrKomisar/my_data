0af504b0280580a020fe2b389fc362ec28491afe
AlexandrKomisar <monolitalex@gmail.com>
2023-03-30 23:12:17 +0300

    add arr water

--- js/my_data.js
+++ js/my_data.js
@@ -18,13 +18,25 @@
 // ];
 
 let water = [
-  2020, [450, 456, 462, 468],
+  2020,
+  [648, 657, 664, 674, 684, 696, 705, 716, 726, 736, 747, 758, 768],
+  2021,
+  [768, 777, 785, 793, 800, 803, 810, 823, 833, 838, 839, 849, 858],
+  2022,
+  [858, 867, 876, 884, 885, 887, 890, 895, 895, 895, 896, 896, 896],
+  2023,
+  [896, 896, 896, 896, , , , , , , , ,],
 ];
+// нульове значення масиву це попередні показники за рік
 
-let sumWater = 0;
+const priceWater = 34.044;
+let sumPriceWater = 0;
 let totalWater = 0;
+
+// рахуємо скільки спожито кубів води
 for (let i = 1; i < water[1].length; i++){
-  totalWater += water[1][i] - water[1][i-1];
+  totalWater += water[1][i] - water[1][i - 1];
+  sumPriceWater= Math.round(totalWater * priceWater)
   console.log(i);
   console.log(water[1][0]);
 
@@ -33,11 +45,9 @@ for (let i = 1; i < water[1].length; i++){
 let out_totalWater = document.getElementById("out_totalWater");
 out_totalWater.innerHTML = totalWater;
 
-// for (let i = 0; i < myData[0][1].length; i++){
-//   console.log(myData[0][1][0]);
-// }
+let out_sumPriceWater = document.getElementById("out_sumPriceWater");
+out_sumPriceWater.innerHTML = sumPriceWater;
 
-// console.log(myData[0][1]);
- console.log(sumWater);
+ console.log(sumPriceWater);
  console.log("totalWater -- "+totalWater);
 
